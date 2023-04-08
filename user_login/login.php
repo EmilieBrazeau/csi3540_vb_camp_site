2d778ae74535e39e263ccd9fd306cd1945348c56
Émilie Brazeau <volleyball.emilie.brazeau@gmail.com>
2023-04-08 16:18:14 -0400

    Login page (html, css, php)

---
+++ user_login/login.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Athlete Login</title>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
+    <div class="container h-100">
+        <div class="d-flex justify-content-center h-100">
+            <div class="user_card">
+                <div class="d-flex justify-content-center">
+                    <div class="brand_logo_container">
+                        <img src="img/volleyball_logo.png" class="brand_logo" alt="Beach Volleyball Camp">
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center form_container">
+                    <form>
+                        <div class="input-group mb-3">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input type="text" name="username" id="username" class="form-control input_user" required>
+                        </div>
+                        <div class="input-group mb-2">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="password" name="password" id="password" class="form-control input_pass" required>
+                        </div>
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" name="rememberme" class="custom-control-input" id="customControlInline">
+                                <label for="customControlInline" class="custom-control-label">Remember me</label>
+                            </div>
+                        </div>
+                    
+                        <div class="d-flex justify-content-center mt-3 login_container">
+                            <button type="button" name="button" id="login" class="btn login_btn">Login</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="mt-4">
+                    <div class="d-flex justify-content-center links">
+                        Don't have an account? <a href="registration.php" class="ml-2">Sign Up</a>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <a href="registration.php">Forgot password?</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
+            integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8="
+            crossorigin="anonymous"></script>
+        
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+    <script>
+        $(function(){
+            $('#login').click(function(e){
+                let valid = this.form.checkValidity();
+
+                if(valid){
+                    let username = $('#username').val();
+                    let password = $('#password').val();
+
+                    e.preventDefault();
+
+                    $.ajax({
+                        type:'POST',
+                        url: 'jslogin.php',
+                        data: {username: username, password: password},
+                        success: function(data){
+                            if($.trim(data) === "1"){
+                                setTimeout(' window.location.href = "index.php"', 2000);
+                            }
+                        },
+                        error: function(data){
+                            alert('There were errors while doind the operation.');
+                        }
+                    });
+                }
+            });
+        });
+        
+
+    </script>
+</body>
+</html>
